845e4fd301070bbc1511503082b5b14dd2345a3b
dmchel <37535673+dmchel@users.noreply.github.com>
2022-02-22 12:08:12 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,28 @@
+package com.leschev.interview;
+
+public class Main {
+
+    static Integer counter;
+
+    static class IncrementingThread extends Thread {
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                counter++;
+            }
+            super.run();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        final IncrementingThread incThread1 = new IncrementingThread();
+        incThread1.start();
+        final IncrementingThread incThread2 = new IncrementingThread();
+        incThread2.start();
+        incThread1.join();
+        incThread2.join();
+        System.out.println("Result=" + counter);
+    }
+
+}
+
